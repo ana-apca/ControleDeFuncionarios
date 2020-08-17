30fd29ea7632c9b6e1976e37af591118d6e04011
Ana Paula Araujo Cavalcante <55284254+ana-apca@users.noreply.github.com>
2020-08-17 14:28:36 -0300

    Atividade-Desafio - Aula 2-1

---
+++ ControleFuncionarios/BLL/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Business
+    {
+        public void Salvar(Pessoa pessoa) { }
+
+    }
+}
